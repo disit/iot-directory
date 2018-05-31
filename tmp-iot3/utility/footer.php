9f3fba19624bcc9e3c1b0d7a39c7b8ab449adad2
root <root@fiware.(none)>
2018-05-31 11:48:53 +0200

    iotDirectory commit

--- tmp-iot3/utility/footer.php
+++
@@ -1,8 +0,0 @@
-   <footer class="py-3 bg-inverse">
-        <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; UNIFI DISIT, UNIMI, EffectiveKnowledge  2018</p>
-        </div>
-        <!-- /.container -->
-    </footer>
-
-    
